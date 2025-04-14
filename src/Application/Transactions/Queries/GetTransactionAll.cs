cbda8480f0c70bd333d1f1bbc79e6f6afaaa8911
_Marchod_ <markjudetapican@gmail.com>
2025-04-14 16:45:47 +0800

    Add files via upload

---
+++ src/Application/Transactions/Queries/GetTransactionAll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Transactions._Dto;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Transactions.Queries.GetTransactionAll
+{
+    public class GetTransactionAllQuery : IRequest<List<TransactionReportDto>>
+    {
+        public class GetTransactionAllQueryHandler : IRequestHandler<GetTransactionAllQuery, List<TransactionReportDto>>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public GetTransactionAllQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<TransactionReportDto>> Handle(GetTransactionAllQuery request, CancellationToken cancellationToken)
+            {
+                var report = await _context.Transactions
+                    .Include(t => t.Kiosk)  
+                    .Select(t => new TransactionReportDto
+                    {
+                        KioskName = t.Kiosk.KioskName,
+                        Category = t.Kiosk.Category,
+                        CreatedDate = t.CreatedDate,  
+                        Amount = t.Amount
+                    })
+                    .ToListAsync(cancellationToken);
+
+                return report;
+            }
+        }
+    }
+}
---
+++ src/Application/Transactions/Queries/GetTransactionReport.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Transactions._Dto;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Transactions.Queries.GetTransactionReport
+{
+    public class GetTransactionReport : IRequest<List<TransactionReportDto>>
+    {
+       
+    }
+
+    public class GetTransactionReportQueryHandler : IRequestHandler<GetTransactionReport, List<TransactionReportDto>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetTransactionReportQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TransactionReportDto>> Handle(GetTransactionReport request, CancellationToken cancellationToken)
+        {
+
+            var report = await _context.Transactions
+                 .GroupBy(t => new
+                 {
+                     t.Kiosk.KioskName,
+                     t.Kiosk.Category,
+                     CreatedDate = t.CreatedDate.Date
+                 })
+                 .Select(g => new TransactionReportDto
+                 {
+                     KioskName = g.Key.KioskName,
+                     Category = g.Key.Category,
+                     CreatedDate = g.Key.CreatedDate,
+                     Amount = g.Sum(t => t.Amount)
+                 })
+                 .OrderBy(r => r.CreatedDate)
+                 .ToListAsync(cancellationToken);
+
+            return report;
+        }
+    }
+}
